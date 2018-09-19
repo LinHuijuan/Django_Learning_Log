c447a7fac531e3dd669f5a21d0964c68d4affaaa
lin_huijuan <1197310697@qq.com>
2018-09-19 20:35:46 +0800

    First version of application moved into github

---
+++ learning_logs/forms.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+from .models import Topic, Entry
+
+class TopicForm(forms.ModelForm):
+    #添加新主题
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text': ''}
+        
+class EntryForm(forms.ModelForm):
+    #添加新条目
+    class Meta:
+        model = Entry
+        fields = ['text']
+        label = {'text':''}
+        widgets = {'text': forms.Textarea(attrs={'cols':80})}
+
